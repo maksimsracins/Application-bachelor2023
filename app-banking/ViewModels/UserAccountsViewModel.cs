5533caa2f2601525bbfb6f0262462b1c2e9a2abd
maksimsracins <56410320+maksimsracins@users.noreply.github.com>
2023-10-23 20:05:39 +0300

    init commit
    1

---
+++ app-banking/ViewModels/UserAccountsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using app_banking.Models;
+
+namespace app_banking.ViewModels
+{
+    public class UserAccountsViewModel
+    {
+        public IEnumerable<User> Users { get; }
+        public IEnumerable<Account> Accounts { get; }
+
+        public UserAccountsViewModel(IEnumerable<User> userRepository,
+            IEnumerable<Account> accountRepository)
+        {
+            Users = userRepository;
+            Accounts = accountRepository;
+        }
+
+    }
+}
