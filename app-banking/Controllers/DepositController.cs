9942623a0e94a157b83732236f73abe46fd6279c
PC <maksims.racins@gmail.com>
2023-10-23 23:10:48 +0300

    web tabs

---
+++ app-banking/Controllers/DepositController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace app_banking.Controllers
+{
+    public class DepositController : Controller
+    {
+        public IActionResult Transfer()
+        {
+            return View();
+        }
+    }
+}
---
+++ app-banking/Controllers/DepositListController.cs
@@ -0,0 +1,24 @@
+﻿using app_banking.Models.Interfaces;
+using app_banking.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace app_banking.Controllers
+{
+    public class DepositListController : Controller
+    {
+        private readonly IAccountRepository _accountRepository;
+        private readonly IUserRepository _userRepository;
+
+        public DepositListController(IAccountRepository accountRepository, IUserRepository userRepository)
+        {
+            _accountRepository = accountRepository;
+            _userRepository = userRepository;
+        }
+
+        public IActionResult ShowDepositList()
+        {
+            UserAccountsViewModel model = new UserAccountsViewModel(_userRepository.GetAllUsers, _accountRepository.GetAllAccounts);
+            return View(model);
+        }
+    }
+}
